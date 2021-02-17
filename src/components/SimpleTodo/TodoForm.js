51e5f3174db83362f8b3e86e624dc85f3451ec13
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-02-17 18:16:30 +0530

    Added the total task completed and total task in the todolist

--- src/components/SimpleTodo/TodoForm.js
+++ src/components/SimpleTodo/TodoForm.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import plus from '../assets/plus.png'
+import TodoList from './TodoList';
 
 export default class TodoForm extends React.Component{
 
 state={
-    text:''
+    id:Math.random(),
+    text:'',
+    complete:false
 }
   
 changeHandler=event=>{
@@ -20,16 +23,36 @@ submitHandler=(event)=>{
         text:this.state.text,
         complete:false
     })
+      
     this.setState({
         text:''
     })
 }
 
-    render(){
+
+  handleFormSubmit = () => {
+    //   const todos=[]
+    // const { id,text, complete } = this.state;
+    // localStorage.setItem('id', id);
+    // localStorage.setItem('text',text);
+    // localStorage.setItem('complete' , complete ? complete : true)
+    //  todos.push(this.state)
+    //  console.log(todos)
+    
+    //test case 1:NO TODOS: 
+    /* if we submit a todo item , there will be a list of todos in the local storage and it will present on the dom
+    */
+   //test case 2:Items already present in local storage
+   /* We will added items to the existing list in the  local storage.  
+   */
+
+  };
+
+ render(){
         return(
-            <form onSubmit={this.submitHandler} className="todo-form">
+            <form onSubmit={this.submitHandler}className="todo-form">
             <input name="text" value={this.state.text} onChange={this.changeHandler} type="text" className="todo-input" placeholder="Add todo"></input>
-            <button onSubmit={this.submitHandler} type="submit" className="add-button"><img src={plus} alt="Add Todo"></img></button>
+            <button  type="submit" className="add-button"><img src={plus} alt="Add Todo"></img></button>
           </form>
       
         )
